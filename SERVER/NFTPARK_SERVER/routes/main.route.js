ab74f4f6f304c2047035c615de4bf47e8f00d092
sinsu <sinsu19012@gmail.com>
2023-01-15 21:44:18 +0900

    feat: rabbitmq 적용

--- SERVER/NFTPARK_SERVER/routes/main.route.js
+++ SERVER/NFTPARK_SERVER/routes/main.route.js
@@ -1,5 +1,8 @@
 const user = require("./user.route");
+const mq = require("../RabbitMQ/rabbitmq-api");
 
 module.exports = (app) => {
+  app.post("/send_msg", mq.send_message);
+  app.get("/get_msg", mq.recv_message);
   app.use("/user", user);
 };
