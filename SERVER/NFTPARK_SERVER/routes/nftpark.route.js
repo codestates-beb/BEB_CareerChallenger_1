74b76a5c36d4a160daef9b854402ccc2575d11eb
sinsu <sinsu19012@gmail.com>
2023-01-23 12:18:04 +0900

    feat: 참여자수 구현

--- SERVER/NFTPARK_SERVER/routes/nftpark.route.js
+++ SERVER/NFTPARK_SERVER/routes/nftpark.route.js
@@ -4,4 +4,5 @@ const middleware = require("../controller/middleware.controller");
 
 router.get("/concertInfo", middleware.redisMiddleware, controller.concertInfo);
 router.get("/ticketInfo", controller.ticketInfo);
+router.get("/detailInfo", middleware.detailMiddleware, controller.detailInfo);
 module.exports = router;
